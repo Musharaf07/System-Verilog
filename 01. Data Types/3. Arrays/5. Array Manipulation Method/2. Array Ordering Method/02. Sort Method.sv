641b8e0dad16e80f451f0badd24b515e0a46b783
Musharaf07 <146569624+Musharaf07@users.noreply.github.com>
2025-07-07 15:55:09 +0530

    Create 02. Sort Method.sv

---
+++ 01. Data Types/3. Arrays/5. Array Manipulation Method/2. Array Ordering Method/02. Sort Method.sv
@@ -0,0 +1,15 @@
+// Array Ordering Method
+//Sort Method
+module sort_method;
+  int dt[];
+  
+  initial begin
+    
+    dt = '{7,3,4,6,2,9,5,8,10,11,12};
+    dt.sort();
+    $display("Sort : %p", dt);
+    
+  end
+endmodule
+
+// Sort : '{2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}
