1c813bacca4dda254ac8797895292d3c17c305f4
Musharaf07 <146569624+Musharaf07@users.noreply.github.com>
2025-09-24 11:02:46 +0530

    Create 02. example_2.v

---
+++ 06. Interprocess Communication/01. SV Event/01. event_trigger_using_@/02. example_2.v
@@ -0,0 +1,27 @@
+// SV Event
+//Event is triggered before waiting for Event trigger using @ 
+module event_exmpl;
+  event ev1;
+  
+  task process1();
+    $display("%0t, Before triggering event",$time);
+    #5;
+    ->ev1;
+    $display("%0t, After triggering event",$time);
+ endtask
+  
+  task process2();
+    $display("%0t, Before Capturing event",$time);
+    #10;
+    @ev1;
+    $display("%0t, After Capturing event",$time);
+  endtask
+  
+  initial begin
+    fork
+      process1();
+      process2();
+    join
+  end
+  
+endmodule
