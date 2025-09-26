86531db5a25a036409a9de1de9dcbc8697e43958
Musharaf07 <146569624+Musharaf07@users.noreply.github.com>
2025-09-26 14:45:14 +0530

    Update 06. Last Index.sv

--- 01. Data Types/3. Arrays/3. Associative Arrays/06. Last Index.sv
+++ 01. Data Types/3. Arrays/3. Associative Arrays/06. Last Index.sv
@@ -1,6 +1,6 @@
 //Associative Array Methods
 //Last Index
-module as_first;
+module as_last;
   int as_array[string];
   string index;
 
@@ -9,6 +9,6 @@ module as_first;
     as_array["A"] = 1;
 
     if (as_array.last(index))
-      $display("First key : %s", index);
+      $display("last key : %s", index);
   end
 endmodule
